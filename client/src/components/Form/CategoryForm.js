825c518a08a40c2f940f3913f269624e5f46b658
MianAteeq-star <mr.unknown5264@gmail.com>
2024-03-24 21:54:59 +0500

    front end crud

---
+++ client/src/components/Form/CategoryForm.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Layout from "../Layout/Layout";
+
+function CategoryForm({handleSubmit, value, setValue}) {
+  return (
+    <>
+      <form
+        action=""
+        onSubmit={handleSubmit}
+        className="mb-6 flex flex-col gap-4"
+      >
+        <div>
+          <label
+            htmlFor="name"
+            className="block mb-1 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Category Name
+          </label>
+          <input
+            type="name"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Men Collection"
+            required
+          />
+        </div>
+        <button
+          type="submit"
+          className="w-full text-black bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Add Category
+        </button>
+      </form>
+    </>
+  );
+}
+
+export default CategoryForm;
--- client/src/pages/Admin/CreateCategory.jsx
+++ client/src/pages/Admin/CreateCategory.jsx
@@ -1,8 +1,68 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../../components/Layout/Layout";
 import AdminMenu from "../../components/Layout/AdminMenu";
+import toast from "react-hot-toast";
+import axios from "axios";
+import { Button, Space, Tooltip } from 'antd';
+import CategoryForm from "../../components/Form/CategoryForm";
+import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
+
 
 function CreateCategory() {
+  const [category, setCategory] = useState([]);
+  const [name,setName ]= useState("")
+
+  const getAllCategory = async (req, res) => {
+    try {
+      const { data } = await axios("/getall-category");
+      if (data.success) {
+        setCategory(data.getAll); // from category controller
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong while getting category");
+    }
+  };
+
+  useEffect(() => {
+    getAllCategory();
+  }, []);
+
+  //  Create a new category
+  const createCategory = async (e) => {
+    e.preventDefault();
+    try {
+      const {data} = await axios.post('/create-category',{name})
+      if(data?.success){
+        toast.success(`${name} ${data.message}`)
+        getAllCategory()
+        setName(" ")
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong while creating category");
+    }
+  };
+
+  // Delete Category
+  const handleDelete = async (id) => {
+    try {
+      const res = await axios.delete(`/delete-category/${id}`);
+      if(res.data.success){
+        toast.success(res.data.message)
+        getAllCategory()
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong while deleting category");
+    }
+  };
+
+  // Update
+
+  const handleUpdate = () => {
+    console.log("updated");
+  };
   return (
     <Layout>
       <div className="flex ">
@@ -14,52 +74,73 @@ function CreateCategory() {
           {/* <!-- component --> */}
           <div className="flex min-h-screen items-center justify-center shadow-2xl">
             <div className="overflow-x-auto">
-              <h2 className="text-center text-4xl mb-4 bg-slate-500 p-4 rounded-lg">Manage Categories</h2>
-              <table className="min-w-full bg-orange-500 shadow-2xl rounded-xl p-4">
+              <h2 className="text-center text-4xl mb-4 bg-slate-500 p-4 rounded-lg">
+                Manage Categories
+              </h2>
+              <CategoryForm handleSubmit={createCategory} value={name} setValue={setName}   />
+              <table className="min-w-full bg-cyan-400 shadow-2xl rounded-xl p-4">
                 <thead>
                   <tr className="bg-blue-gray-100 text-gray-700">
+                    <th className="py-3 px-4 text-left"> #</th>
                     <th className="py-3 px-4 text-left"> Name</th>
 
                     <th className="py-3 px-4 text-left">Action</th>
                   </tr>
                 </thead>
                 <tbody className="text-blue-gray-900">
-                  <tr className="border-b border-blue-gray-200">
-                    <td className="py-3 px-4">Company A</td>
+                  {category.map((categ, i) => (
+                    <>
 
-                    <td className="py-3 px-4">
-                    
+                      <tr className="border-b border-blue-gray-200">
+                      <th>{ i + 1}</th>
+                        <td key={categ._id} className="py-3 px-4">
+                          {categ.name}
+                        </td>
 
+                        <td>
+                          <Space>
+                            <Tooltip title="Delete" color='red'><Button className="text-red-600 border-black" icon={<DeleteOutlined />} onClick={() => { handleDelete(categ._id) }} /></Tooltip>
+                            <Tooltip title="Edit"><Button type="primary" className="text-blue-500 border-black" icon={<EditOutlined />}  onClick={handleUpdate} /></Tooltip>
+                          </Space>
+                        </td>
 
-<div className="group inline-block">
-  <button className="outline-none focus:outline-none border px-3 py-1 bg-white rounded-sm flex items-center min-w-14">
-    <span className="pr-1 flex-1 font-medium text-blue-600 hover:text-blue-800">Edit</span>
-    <span>
-      <svg className="fill-current h-4 w-4 transform group-hover:-rotate-180
-  transition duration-150 ease-in-out" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-        <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-      </svg>
-    </span>
-  </button>
-  <ul className="bg-white border rounded-sm transform scale-0 group-hover:scale-100 absolute 
-  transition duration-150 ease-in-out origin-top min-w-32">
-    <li className="rounded-sm px-3 py-1 hover:bg-blue-600 cursor-pointer font-medium">Update</li>
-    <li className="rounded-sm px-3 py-1 cursor-pointer hover:bg-red-600 font-medium">Delete</li>
- 
-  </ul>
-</div>
-
-
-
-
-
-                    </td>
-                  </tr>
-                  
-                  
+                        {/* <td className="py-3 px-4">
+                          <div className="group inline-block">
+                            <button className="outline-none focus:outline-none border px-3 py-1 bg-white rounded-sm flex items-center min-w-14">
+                              <span className="pr-1 flex-1 font-medium text-blue-600 hover:text-blue-800">
+                                Edit
+                              </span>
+                              <span>
+                                <svg
+                                  className="fill-current h-4 w-4 transform group-hover:-rotate-180 transition duration-150 ease-in-out"
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  viewBox="0 0 20 20"
+                                >
+                                  <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                                </svg>
+                              </span>
+                            </button>
+                            <ul className="bg-white border rounded-sm transform scale-0 group-hover:scale-100 absolute  transition duration-150 ease-in-out origin-top min-w-32">
+                              <li
+                                className="rounded-sm px-3 py-1 hover:bg-blue-600 cursor-pointer font-medium"
+                                onClick={handleUpdate}
+                              >
+                                Update
+                              </li>
+                              <li
+                                className="rounded-sm px-3 py-1 cursor-pointer hover:bg-red-600 font-medium"
+                                onClick={()=> handleDelete(categ._id)}
+                              >
+                                Delete
+                              </li>
+                            </ul>
+                          </div>
+                        </td> */}
+                      </tr>
+                    </>
+                  ))}
                 </tbody>
               </table>
-             
             </div>
           </div>
         </div>
